79a91ea08ae8cbcda52d1f92a5a92f5fb15d6e89
MemoriesOff <496746220@qq.com>
2015-10-03 23:13:30 +0800

    Update post.js

--- static/js/post.js
+++ static/js/post.js
@@ -29,11 +29,12 @@ function share(){
 }
 
 
-function disqus(){
-    /* * * CONFIGURATION VARIABLES * * */
-    var disqus_shortname = '{{site.disqus_shortname}}';
-
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    document.getElementsByTagName("script")[0].parentNode.appendChild(dsq);
-}
+var duoshuoQuery = {short_name:"acgtrip"};
+	(function() {
+		var ds = document.createElement('script');
+		ds.type = 'text/javascript';ds.async = true;
+		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+		ds.charset = 'UTF-8';
+		(document.getElementsByTagName('head')[0] 
+		 || document.getElementsByTagName('body')[0]).appendChild(ds);
+	})();
